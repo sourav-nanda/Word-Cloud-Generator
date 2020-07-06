65b98f23fd5dbf5d0e62117c375aa51a9b4db70c
sourav-nanda <67404111+sourav-nanda@users.noreply.github.com>
2020-07-06 23:27:31 +0530

    Added support for image mask

--- word_cloud_generator.py
+++ word_cloud_generator.py
@@ -1,28 +1,28 @@
-#C:\Users\Saroj\AppData\Roaming\Python\Python37\Scripts
 from wordcloud import WordCloud
 import warnings
 import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
-from PIL import Image
 
 warnings='ignore'
+def gen():
+    st.title('WORD CLOUD GENERATOR')
+    st.subheader('Enter your text here')
+    text=st.text_area('')
+    
+    img_types=['jpeg','jpg','png','webp']
+    img_file=st.file_uploader('Upload your image',type=img_types)
+    st.write(supported file types:''jpeg','jpg','png','webp')
 
-st.title('WORD CLOUD GENERATOR')
+    img=plt.imread(img_file)
+    mask=np.array(img)
+    size=st.slider('Adjust the size of the wordcloud',value=(1,10,value=(5,14))
+    fig=plt.figure(figsize=(size,size))
+    wc=WordCloud(background_color='white',mask=mask)
+    
+    return wc.generate(text)
 
-st.subheader('Enter some text')
-text=st.text_area('')
-
-#st.subheader('Enter the Path/URL of the image mask')
-#path=st.text_area('.')
-
-#img=Image.open(r'F:\8k wallpapers\104765.jpg')
-#mask=np.array(img)
-fig=plt.figure(figsize=(8,8))
-wc=WordCloud(background_color='white')
-wc.generate(text)
-
-plt.imshow(wc,cmap='viridis',interpolation='nearest')
+plt.imshow(gen(),cmap='viridis',interpolation='bilinear')
 plt.axis('off')
 plt.show()
 
