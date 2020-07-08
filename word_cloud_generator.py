c3658e2b53efafdd13029a19c3289ef21f78080c
souravnanda <souravnanda412gmail.com>
2020-07-08 16:32:56 +0530

    Bug Fixes,New addition
    Fixed Image mask functionality and added support for color masking

--- word_cloud_generator.py
+++ word_cloud_generator.py
@@ -1,28 +1,36 @@
-from wordcloud import WordCloud
+from wordcloud import WordCloud,ImageColorGenerator
 import warnings
 import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
+from PIL import Image
 
 warnings='ignore'
 def gen():
+    global size
+    global wc
+    global img_col
     st.title('WORD CLOUD GENERATOR')
     st.subheader('Enter your text here')
     text=st.text_area('')
     
     img_types=['jpeg','jpg','png','webp']
     img_file=st.file_uploader('Upload your image',type=img_types)
-    st.write('supported file types:','jpeg','jpg','png','webp')
+    st.write('Supported file types are ','jpeg,','jpg,','png,','webp')
 
-    img=plt.imread(img_file)
+    img=Image.open(img_file)
     mask=np.array(img)
-    size=st.slider('Adjust the size of the wordcloud',value=(1,10,value=(5,14))
-    fig=plt.figure(figsize=(size,size))
-    wc=WordCloud(background_color='white',mask=mask)
+    size=st.slider('Adjust the resolution of the wordcloud',1,10)
+    
+    
+    wc=WordCloud(background_color='white',mask=mask,height=1920,width=1080)
+    
+    img_col=ImageColorGenerator(mask)
     
     return wc.generate(text)
-
-plt.imshow(gen(),cmap='viridis',interpolation='bilinear')
+gen()
+fig=plt.figure(figsize=[size,size])
+plt.imshow(wc.recolor(color_func=img_col),cmap='viridis',interpolation='bilinear')
 plt.axis('off')
 plt.show()
 
